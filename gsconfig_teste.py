b838c915a42b91db4c085960333bb414e29e2642
Gabriele Camara <36965321+GabrieleCamara@users.noreply.github.com>
2019-10-10 10:29:38 -0300

    Update gsconfig_teste.py

--- gsconfig_teste.py
+++ gsconfig_teste.py
@@ -3,16 +3,16 @@ from geoserver.catalog import Catalog
 cat = Catalog("http://localhost:8082/geoserver/rest", username="admin", password="geoserver")
 
 # Criando um workspace
-#ws = cat.create_workspace('gsc_test','http://localhost:8082/geoserver/testWs')
+ws = cat.create_workspace('gsc_test','http://localhost:8082/geoserver/testWs')
 
 # Criando um store
 ds = cat.create_datastore('bdg_gsc','gsc_test')
-#ds.connection_parameters.update(host='localhost', port='5432', database='bdg_gsc', user='user', passwd='user', dbtype='postgis', schema='public')
-#cat.save(ds)
+ds.connection_parameters.update(host='localhost', port='5432', database='bdg_gsc', user='user', passwd='user', dbtype='postgis', schema='public')
+cat.save(ds)
 
 # Adicionando layers
-#ft = cat.publish_featuretype('mun_pr', ds, 'EPSG:4674', srs='EPSG:4674')
-#ft1 = cat.publish_featuretype('escolas_pr', ds, 'EPSG:4674', srs='EPSG:4674')
+ft = cat.publish_featuretype('mun_pr', ds, 'EPSG:4674', srs='EPSG:4674')
+ft1 = cat.publish_featuretype('escolas_pr', ds, 'EPSG:4674', srs='EPSG:4674')
 
 # Adicionando styles - nao adiciona sld completo
 #with open("/home/user/gsc_style.sld") as f:
